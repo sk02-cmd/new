ea619d2f1c37757ad3d872303c66d254b40786b8
sk02-cmd <shwetakanakdande02@gmail.com>
2025-07-29 23:16:53 +0530

    removing

--- main.tf
+++ main.tf
@@ -2,9 +2,3 @@
 resource "aws_s3_bucket" "first-bucket" {
   bucket = "omgggg"
 }
-
-resource "aws_s3_bucket" "second-bucket"{
-  bucket = "my-firsteverbucket-1234"  # Bucket name must be globally unique
- 
- 
-}
