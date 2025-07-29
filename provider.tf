21ea4d3ccea0bd674b10bcb82390e4855499832b
sk02-cmd <shwetakanakdande02@gmail.com>
2025-07-29 23:42:35 +0530

    wohho

--- provider.tf
+++ provider.tf
@@ -1,3 +1,7 @@
-provider "aws" {
-  region = "us-west-2"
-  }
+
+
+  provider "aws" {
+  region                  = "us-west-2"
+  shared_credentials_files = ["C:/Users/spkan/.aws/credentials"]
+  profile                 = "default" 
+}
