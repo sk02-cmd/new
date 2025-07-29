09a54732ce0136491c9079e14fe3bdf270f6fa17
ShripadK29 <shripad.kanakdande29@gmail.com>
2025-07-29 23:06:25 +0530

    adidng again

--- main.tf
+++ main.tf
@@ -2,3 +2,9 @@
 resource "aws_s3_bucket" "first-bucket" {
   bucket = "omgggg"
 }
+
+resource "aws_s3_bucket" "second-bucket"{
+  bucket = "my-firsteverbucket-1234"  # Bucket name must be globally unique
+ 
+ 
+}
